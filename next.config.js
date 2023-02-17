0bc0ab7edeebfd1269829c4056551c59337905c5
abelpz <abelper54@gmail.com>
2023-02-16 22:27:57 -0400

    2 files added, 1 deleted

---
+++ next.config.js
@@ -0,0 +1,9 @@
+const withPWA = require('next-pwa')({
+  disable: process.env.NODE_ENV === 'development',
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+});
+module.exports = withPWA({
+  reactStrictMode: true,
+});
